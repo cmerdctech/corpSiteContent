06d3d8c243672878e2dff5fe37f1565d272e5c66
TQ White II <tq@justkidding.com>
2013-03-11 22:11:44 -0500

    getting close

---
+++ imaging/_GLOBAL/JS/bidding.js
@@ -0,0 +1,21 @@
+
+$('.chalkField').hover(
+
+function(event){ //handerlIn
+
+	$(event.target).css({
+	background:'#3f4e46'
+	});
+
+},
+
+function(event){ //handerlOut
+
+$(event.target).css({
+	background:'none'
+	});
+
+}
+
+);
+
