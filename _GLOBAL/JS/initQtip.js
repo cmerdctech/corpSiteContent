5b7fd26e5b7e5c4351d03a19f1ba437ea2c49623
TQ White II <tq@justkidding.com>
2013-03-18 10:32:23 -0500

    better badges, many other things

---
+++ _GLOBAL/JS/initQtip.js
@@ -0,0 +1,18 @@
+$('[title]').qtip({
+	id: 'myTooltip',
+// 	content: {
+// 		text: 'My ID is #ui-tooltip-myTooltip'
+// 	},
+	style: {
+		classes: 'ui-tooltip-blue ui-tooltip-shadow ui-tooltip-tipsy'
+	},
+	position: {
+		my: 'top left',
+		at: "bottom center"
+	},
+	show: {
+		effect: function(offset) {
+			$(this).slideDown(100); // "this" refers to the tooltip
+		}
+	}
+});
