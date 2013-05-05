e5a479313d7dbc478df28a71d793c21aa06ae886
TQ White II <tq@justkidding.com>
2013-05-05 14:09:53 -0500

    more ten sigma store

--- traxStore/catalog/catalogDisplayTemplates/transformations.php
+++ traxStore/catalog/catalogDisplayTemplates/transformations.php
@@ -4,3 +4,17 @@ $transformations['fancyName']=function($itemRec, $referenceDataTagList){
 	$out=isset($itemRec['name'])?'**'.$itemRec['name']."**({$referenceDataTagList['categoryInfo:name']})":"!isset(\$itemRec['name'])"; 
 	return $out;
 };
+$transformations['redemptionUrl']=function($itemRec, $referenceDataTagList){
+
+	if (!isset($referenceDataTagList['cardData:cardNumber'])){return "!isset(\$referenceDataTagList['cardData:cardNumber']";}
+	if (!isset($referenceDataTagList['redemption:demoUrl'])){return "!isset(\$referenceDataTagList['redemption:demoUrl']";}
+	if (!isset($referenceDataTagList['redemption:productionUrl'])){return "!isset(\$referenceDataTagList['redemption:productionUrl']";}
+
+	if (str_replace('8882', '', $referenceDataTagList['cardData:cardNumber'])){
+		return $referenceDataTagList['redemption:demoUrl'];
+	}
+	else{
+		return $referenceDataTagList['redemption:productionUrl'];
+	}
+
+};
