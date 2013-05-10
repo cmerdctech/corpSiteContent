31ac4fca9ee2b3ee8dd2b1c74898747b493f71e5
TQ White II <tq@justkidding.com>
2013-05-10 12:17:53 -0500

    urls go right places, access prov server ok, access pay server ok

--- traxStore/catalog/catalogDisplayTemplates/transformations.php
+++ traxStore/catalog/catalogDisplayTemplates/transformations.php
@@ -6,32 +6,50 @@ $transformations['fancyName']=function($itemRec, $referenceDataTagList){
 	$out=isset($itemRec['name'])?'**'.$itemRec['name']."**({$referenceDataTagList['categoryInfo:name']})":"!isset(\$itemRec['name'])"; 
 	return $out;
 };
-$transformations['redemptionUrl']=function($itemRec, $referenceDataTagList){
-	$fieldName='redemption:demoUrl'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
-	$fieldName='redemption:productionUrl'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+		
+$transformations['formattedGrandTotal']=function($itemRec, $referenceDataTagList){
+	$fieldName='priceSummary:grandTotal'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+	return number_format($referenceDataTagList['priceSummary:grandTotal'], 2);
+};
+
+$transformations['formattedTax']=function($itemRec, $referenceDataTagList){
+	$fieldName='priceSummary:tax'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+	return number_format($referenceDataTagList['priceSummary:tax'], 2);
+};
+
+$transformations['formattedSubtotal']=function($itemRec, $referenceDataTagList){
+	$fieldName='priceSummary:subTotal'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+	return number_format($referenceDataTagList['priceSummary:subTotal'], 2);
+};
+
+
+
+$transformations['redemptionUrlNew']=function($itemRec, $referenceDataTagList){
+	$fieldName='redemption:demoUrlNew'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+	$fieldName='redemption:productionUrlNew'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
 	$fieldName='provisionServer'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
 
 
 	if ($referenceDataTagList['provisionServer']=='demo'){
-		return $referenceDataTagList['redemption:demoUrl'];
+		return $referenceDataTagList['redemption:demoUrlNew'];
 	}
 	else{
-		return $referenceDataTagList['redemption:productionUrl'];
+		return $referenceDataTagList['redemption:productionUrlNew'];
 	}
 
 };
-		
-		$transformations['formattedGrandTotal']=function($itemRec, $referenceDataTagList){
-			$fieldName='priceSummary:grandTotal'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
-			return number_format($referenceDataTagList['priceSummary:grandTotal'], 2);
-		};
-		
-		$transformations['formattedTax']=function($itemRec, $referenceDataTagList){
-			$fieldName='priceSummary:tax'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
-			return number_format($referenceDataTagList['priceSummary:tax'], 2);
-		};
-		
-		$transformations['formattedSubtotal']=function($itemRec, $referenceDataTagList){
-			$fieldName='priceSummary:subTotal'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
-			return number_format($referenceDataTagList['priceSummary:subTotal'], 2);
-		};
+
+$transformations['redemptionUrlExisting']=function($itemRec, $referenceDataTagList){
+	$fieldName='redemption:demoUrlExisting'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+	$fieldName='redemption:productionUrlExisting'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+	$fieldName='provisionServer'; if (!isset($referenceDataTagList[$fieldName])){return "!isset(\$referenceDataTagList[$fieldName]";}
+
+
+	if ($referenceDataTagList['provisionServer']=='demo'){
+		return $referenceDataTagList['redemption:demoUrlExisting'];
+	}
+	else{
+		return $referenceDataTagList['redemption:productionUrlExisting'];
+	}
+
+};
