2f615a3eb342c0bc2fe0827a631ae72313bdd023
TQ White II <tq@justkidding.com>
2013-05-10 14:22:22 -0500

    better highlight processing errors

--- traxStore/_GLOBAL/JS/cartButton.js
+++ traxStore/_GLOBAL/JS/cartButton.js
@@ -4,6 +4,8 @@ $('.addToCartButton').click(function(eventObj){
 		$('.mainContentContainer').widgets_simple_store_main('addEventObjToCart', eventObj);
 		$(eventObj.target).parent().css({opacity:0.25}).animate({opacity: 1.0}, 500);
 		$('.viewCartButton').show();
+		
+		$('.mainContentContainer').widgets_simple_store_main('display', {catalogUrl:''});
 		}
 	);
 
