c022f35d8f79da76ac61bd451e08daabad1f8cd9
TQ White II <tq@justkidding.com>
2013-11-19 16:08:57 -0600

    removed a zillion redundant contectForm.js files

--- supplies/_GLOBAL/JS/contactForm.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-		$('.contactFormButton').click(function(event){	
-			$('body').widgets_features_contact_form({
-				event:event,
-				parameterFileName:'contactForms.ini'
-			});
-		});
-	});
