305cfe40c051c9d999b6d628cc424a337bd041ec
TQ White II <tq@justkidding.com>
2014-07-22 13:00:05 -0500

    next rev of products

--- traxStore/mgmtScriptFragments.bash
+++ traxStore/mgmtScriptFragments.bash
@@ -5,12 +5,12 @@
 
 cd /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore
 
-rm -r /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore/_default/productSpecs/*
+rm -r /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore/all/productSpecs/*
 
-generateFilesFromData.js --forReal --verbose spreadsheetData/productSpecs070814.txt _default/productSpecs ini
+generateFilesFromData.js --forReal --verbose spreadsheetData/productSpecs.txt all/productSpecs ini
 
 
-generateFilesFromData.js --verbose spreadsheetData/productSpecs070814.txt _default/productSpecs ini
+generateFilesFromData.js --verbose spreadsheetData/productSpecs.txt all/productSpecs ini
 
 
 
@@ -19,14 +19,19 @@ generateFilesFromData.js --verbose spreadsheetData/productSpecs070814.txt _defau
 
 cd /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore
 
+open /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore/tmp/catalogDisplayTemplates
+
 rm -r /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore/tmp/catalogDisplayTemplates/*
 
-generateFilesFromData.js --forReal --verbose --template _default/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt _default/categoryTemplates html
+generateFilesFromData.js --forReal --verbose --template all/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt all/categoryTemplates html
 
 
-generateFilesFromData.js --verbose --template _default/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt _default/categoryTemplates html
+generateFilesFromData.js --verbose --template all/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt all/categoryTemplates html
 
 
 ===============================================
 
-copyFiles.js -verbose pageCopySpecs.txt _default/productSpecs tmp ini
+copyFiles.js -verbose --forReal spreadsheetData/pageCopySpecs.txt all/productSpecs tmp ini
+
+
+copyFiles.js -verbose spreadsheetData/pageCopySpecs.txt all/productSpecs tmp ini
