7c9693ac400a50d9e9933e3ad4d7279e690326d4
TQ White II <tq@justkidding.com>
2014-07-29 15:24:38 -0500

    revised trax store footer, misc cosmetic, revised products

--- traxStore/_GLOBAL/JS/cartButton.js
+++ traxStore/_GLOBAL/JS/cartButton.js
@@ -1,11 +1,16 @@
-$('.mainContentContainer').widgets_simple_store_main('showViewCartButtonIfNeeded', '.viewCartButton');
+var mainContentContainer=$('.mainContentContainer'),
+	testIfStore=$('.catalogContentContainer');
+
+console.log('testIfStore.length='+testIfStore.length+'\n');
+if (testIfStore.length>0){
+mainContentContainer.widgets_simple_store_main('showViewCartButtonIfNeeded', '.viewCartButton');
 
 $('.addToCartButton').click(function(eventObj){
-		$('.mainContentContainer').widgets_simple_store_main('addEventObjToCart', eventObj);
+		mainContentContainer.widgets_simple_store_main('addEventObjToCart', eventObj);
 	//	$(eventObj.target).parent().css({opacity:0.25}).animate({opacity: 1.0}, 500);
 		$('.viewCartButton').show();
 		
- 		$('.mainContentContainer')
+ 		mainContentContainer
 // 	//		.widgets_simple_store_main('display', {catalogUrl:''})
  			.widgets_simple_store_main('showCartPopup');
 		
@@ -15,21 +20,9 @@ $('.addToCartButton').click(function(eventObj){
 
 $('.viewCartButton').click(
 	function(){
-		$('.mainContentContainer').widgets_simple_store_main('display', {catalogUrl:''});
+		mainContentContainer.widgets_simple_store_main('display', {catalogUrl:''});
 	}
 );
 
-// $('.mainContentContainer').widgets_simple_store_main('display', {catalogUrl:''});
-// $('body').prepend("<div style='float:right;margin:20px 40px;background:white;padding:10px;color:red;font-size:14pt;'>Jammed straight to checkout for TQ's convenience</div>")
 
-
-// 			$('body').widgets_tools_ui_modal_screen({
-// 				employerSender:function(){},
-// 				controls:{
-// 					message:"<img src='../images/debbieWhiteDress.jpg'>",
-// 					version:2,
-// 					backgroundClickFunction:function(){ $(this).hide(); },
-// 			//		fadeParms:{duration:3000},
-// 					messagePosition:'window'
-// 				}
-// 			});
+}
--- traxStore/mgmtScriptFragments.bash
+++ traxStore/mgmtScriptFragments.bash
@@ -20,6 +20,8 @@ generateFilesFromData.js --forReal --verbose spreadsheetData/productSpecs.txt al
 cd /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore
 
 generateFilesFromData.js --forReal --verbose --template all/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt all/categoryTemplates html
+generateFilesFromData.js --forReal --verbose --template all/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt behavior/categoryTemplates html
+generateFilesFromData.js --forReal --verbose --template all/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt transition/categoryTemplates html
 
 
 //generateFilesFromData.js --verbose --template all/categoryTemplatesDefaultFiles/categorySubstitutionTemplate.html spreadsheetData/categoryTemplateData.txt all/categoryTemplates html
@@ -34,8 +36,16 @@ copyFiles.js -verbose --forReal spreadsheetData/pageCopySpecs.txt all/productSpe
 rm -r /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore/behavior/productSpecs/*
 rm -r /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/traxStore/transition/productSpecs/*
 
-mv tmp/TransitionProducts/* behavior/productSpecs/
-mv tmp/BehaviorProducts/* transition/productSpecs/
+mv tmp/BehaviorProducts/* behavior/productSpecs/
+mv tmp/TransitionProducts/* transition/productSpecs/
 
 
 //copyFiles.js -verbose spreadsheetData/pageCopySpecs.txt all/productSpecs tmp ini
+
+===============================================
+
+cd /Volumes/qubuntuFileServer/cmerdc/corporateMultisite/content/
+
+git status
+
+git commit -am"changed products in store"
