a5e15d7439f09a74402c408af7022e052dfd05b3
TQ White II <tq@justkidding.com>
2015-09-18 16:49:55 -0500

    working on IE function

--- planningDiscipline/_default/JS/infoPanels.js
+++ planningDiscipline/_default/JS/infoPanels.js
@@ -5,33 +5,33 @@ NOTE: fidgets_control_vertical_structure is instantiated in featureControl becau
 needs to be coordinated to not do bookmarks before the carousel panel is set up
 */
 
-	var animate = function(eventObj) {
-		var target = $(eventObj.target);
-		target.find('.mobile').addClass('animated slideInRight')
-	}
-
-	var mobile = $('.mobile'),
-		waitingForAction = false,
-		animationClasses='animated flipInX';
-
-	var addAnimation = function(eventObj) {
-		eventObj.stopPropagation();
-		var target = $(eventObj.target);
-
-				target.data('alreadyAnimated', true).addClass('opaque').addClass(animationClasses)
-				.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', removeAnimation)
-	};
-
-	var removeAnimation = function(eventObj) {
-		eventObj.stopPropagation();
-		var target = $(eventObj.target);
-		target.removeClass(animationClasses)
-	};
-
-		mobile
-			.bind('enterviewport', addAnimation)
-//			.bind('leaveviewport', removeAnimation)
-			.bullseye();
+// 	var animate = function(eventObj) {
+// 		var target = $(eventObj.target);
+// 		target.find('.mobile').addClass('animated slideInRight')
+// 	}
+// 
+// 	var mobile = $('.mobile'),
+// 		waitingForAction = false,
+// 		animationClasses='animated flipInX';
+// 
+// 	var addAnimation = function(eventObj) {
+// 		eventObj.stopPropagation();
+// 		var target = $(eventObj.target);
+// 
+// 				target.data('alreadyAnimated', true).addClass('opaque').addClass(animationClasses)
+// 				.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', removeAnimation)
+// 	};
+// 
+// 	var removeAnimation = function(eventObj) {
+// 		eventObj.stopPropagation();
+// 		var target = $(eventObj.target);
+// 		target.removeClass(animationClasses)
+// 	};
+// 
+// 		mobile
+// 			.bind('enterviewport', addAnimation)
+// //			.bind('leaveviewport', removeAnimation)
+// 			.bullseye();
 
 })(jQuery);
 
