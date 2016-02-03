d60c384319a635be313994698f073437e4e60cb3
TQ White II <tq@justkidding.com>
2016-02-03 10:20:51 -0600

    pictures are all purchased and without watermarks

--- planningDiscipline/_default/JS/contactForm.js
+++ planningDiscipline/_default/JS/contactForm.js
@@ -11,8 +11,20 @@ $('.infusionsoftButtonIdClass').click(function(event){
 var infusionWindow=window.open('', 'infusionWindow', "height=516,width=486,modal=yes,alwaysRaised=yes");
 infusionWindow.screenX=window.screenX+100;
 infusionWindow.screenY=window.screenY+200;
+var formName=$(this).attr('formName');
 
-infusionWindow.document.write('<div><xxx type="text/javascript" src="https://qa202.infusionsoft.com/app/form/iframe/9eb9cdd785d00fdffe5813128d432bcc"></xxx></div>'.replace(/xxx/g, 'script'));
+infusionWindow.document.write(('<div><xxx type="text/javascript" src="https://qa202.infusionsoft.com/app/form/iframe/'+formName+'"></xxx></div>').replace(/xxx/g, 'script')); //needs the parents so .replace() works
+
+var popup=$('#contactPopup');
+if (!popup.length){
+$('body').append(" <div id='contactPopup' class='shadow1' style='background:white;border:2pt solid #666;position:absolute;top:30vh;left:30vw;width:40vw;height:40vh;padding:5%;font-size:150%;'> The contact form appears in a pop up browser window. If it doesn't appear immediately, you might need to tell your browser to allow it to show. </div> ");
+}
+else{
+popup.show();
+}
+
+$('#contactPopup').fadeOut(15000);
+
 
 // 	var indicatorFunction = function() {
 // 
