4dcc503cf461baf505dc585f016b85c12fe6944b
TQ White II <tq@justkidding.com>
2016-02-05 11:56:20 -0600

    updated the lcap maestro pix, etc, ready for limited production

--- planningDiscipline/test/JS/showQueryIndicator.js
+++
@@ -1,8 +0,0 @@
-if (window.location.href.match(/local/)){
-$('body').append("<div class='indicatorPlus'></div><div class='indicatorMinus'></div>");
-}
-
-if (window.location.href.match(/test/)){
-$('body').append("<div style='position:fixed;top:1%;left:1%;background:yellow;color:black;font-weight:bold;width:200px;text-align:center;'>TEST PAGE</div>");
-}
-
